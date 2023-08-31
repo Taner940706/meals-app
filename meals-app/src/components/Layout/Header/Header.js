280aa885cad9b1c43c4300de6deaed218ddbcd34
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-08-31 21:09:17 +0300

    header cart button component

--- meals-app/src/components/Layout/Header/Header.js
+++ meals-app/src/components/Layout/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './Header.css'
 import mealImage from '../../../assets/meals.jpg'
+import HeaderCartButton from '../HeaderCartButton/HeaderCartButton';
 
 export default function Header() {
   return (
     <>
     <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
     </header>
     <div className={classes['main-image']}>
         <img src={mealImage} alt="Meals image, main image" />
