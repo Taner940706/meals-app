1cc46e3d80293ce685e046b4bcb44319225d5df6
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-09-01 20:18:50 +0300

    meal items components

--- meals-app/src/components/Meals/AvailableMeals.js
+++ meals-app/src/components/Meals/AvailableMeals.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import classes from './AvailableMeals.module.css'
 import DUMMY_MEALS from '../../assets/data/data'
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 export default function AvailableMeals() {
-    const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const mealList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
 
   return (
     <section className={classes.meals}>
+      <Card>
     <ul>
         {mealList}
     </ul>
+    </Card>
     </section>
   )
 }
