a58ee4e99dc2e229990087198d1e2dddbc65a9b6
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-09-03 14:21:25 +0300

    adding cart context

--- meals-app/src/App.js
+++ meals-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 function App() {
 
@@ -18,13 +19,13 @@ function App() {
 
 
   return (
-    <>
+    <CartProvider>
     {cartIsShown && <Cart onClose={hideCartHnadler} />}
     <Header onShowCart={showCartHnadler} />
     <main>
       <Meals />
     </main>
-    </>
+    </CartProvider>
   );
 }
 
