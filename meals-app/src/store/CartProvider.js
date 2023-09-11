c17697578e1adf5b9a82db118b1a93fca0449659
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-09-11 20:34:09 +0300

    add cart reducer

--- meals-app/src/store/CartProvider.js
+++ meals-app/src/store/CartProvider.js
@@ -1,15 +1,41 @@
 import React from 'react'
+import { useReducer } from 'react';
 import CartContext from './cart-context'
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD'){
+    const updatedItems = state.items.concat(action.item);
+    const updateTotalAmount = state.totalAmount + action.item.price*action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updateTotalAmount
+    };
+  }
+  return defaultCartState;
+}
+
 
 export default function CartProvider(props) {
 
-    const addItemToCartHandler = (item) => {};
+    const [cartState, dispathCartAction] = useReducer(cartReducer, defaultCartState);
 
-    const removeItemFromCartHnadler = (id) => {};
+    const addItemToCartHandler = (item) => {
+      dispathCartAction({type: 'ADD', item: item})
+    };
+
+    const removeItemFromCartHnadler = (id) => {
+      dispathCartAction({type: 'REMOVE', id: id})
+    };
+
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHnadler,
     }
