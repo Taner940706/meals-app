9513ade81af0dd66d44e2aa17310037110a7a80e
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-09-11 20:58:50 +0300

    fers and forward refs

--- meals-app/src/components/Meals/MealItem/MealItemForm.js
+++ meals-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { useRef, useState } from 'react'
 import Input from '../../UI/Input'
 import classes from './MealItemForm.module.css'
 
 export default function MealItemForm(props) {
+
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = event =>{
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount
+
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form classname={classes.form}>
-        <Input label="Amount" input={{
+    <form classname={classes.form} onSubmit={su``}>
+        <Input 
+        ref={amountInputRef}
+        label="Amount" input={{
             id: 'amount_' + props.id,
             type: 'number',
             min: '1',
@@ -14,6 +35,7 @@ export default function MealItemForm(props) {
             defaultValue: '1'
         }}/>
         <button>+ Add</button>
+        {!amountIsValid && <p>Please enter a valid amount (1-5</p>}
     </form>
   )
 }
