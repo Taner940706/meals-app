4b6bfba88657ab6bcf3d40a6f2b5fc9844606b0a
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-09-26 14:21:08 +0300

    create firebase database and fetch meals vie http

--- meals-app/src/components/Meals/AvailableMeals.js
+++ meals-app/src/components/Meals/AvailableMeals.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import {useEffect, useState} from 'react'
 import classes from './AvailableMeals.module.css'
-import DUMMY_MEALS from '../../assets/data/data'
+// import DUMMY_MEALS from '../../assets/data/data'  when data is DUMMY
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
 export default function AvailableMeals() {
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://console.firebase.google.com/project/meals-app-bc976/database/meals-app-bc976-default-rtdb/data/~2F.meals.js')
+      const responseData = await response.json();
+
+      const loadMeals = []
+      for (const key in responseData){
+        loadMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+
+        })
+      }
+    }
+  }, [])
     const mealList = DUMMY_MEALS.map(meal => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
 
   return (
